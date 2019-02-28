c77e2c8af7cdc050a1b438b9ef437c434df8c1ab
Stanislav Goncharov <stanislav.goncharov@globallogic.com>
2019-02-28 15:17:03 +0200

    Fix CPU Variant
    Signed-off-by: Stanislav Goncharov <stanislav.goncharov@globallogic.com>

--- plus2e/BoardConfig.mk
+++ plus2e/BoardConfig.mk
@@ -20,7 +20,8 @@ TARGET_NO_KERNEL := true
 TARGET_ARCH := arm
 
 TARGET_ARCH_VARIANT := armv7-a
-TARGET_CPU_VARIANT := cortex-a7
+#TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_VARIANT := generic
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 
