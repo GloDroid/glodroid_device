e75024d7addca52573a5af10d108d0f30a199fdb
Stanislav Goncharov <stanislav.goncharov@globallogic.com>
2019-06-04 14:20:43 +0300

    device.mki: Add AOSP Configstore HAL Service to Android build configuration
    Signed-off-by: Stanislav Goncharov <stanislav.goncharov@globallogic.com>

--- plus2e/device.mk
+++ plus2e/device.mk
@@ -48,3 +48,8 @@ PRODUCT_PACKAGES += \
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
+
+# Configstore HAL
+PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.1-service
+
