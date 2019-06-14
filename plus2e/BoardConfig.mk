88e02cc75f3db6125d66aecb29a352adfaeed323
Stanislav Goncharov <stanislav.goncharov@globallogic.com>
2019-06-14 12:28:12 +0300

    BoardConfig.mk: preliminary changes for Android build
    Signed-off-by: Stanislav Goncharov <stanislav.goncharov@globallogic.com>

--- plus2e/BoardConfig.mk
+++ plus2e/BoardConfig.mk
@@ -55,9 +55,9 @@ BOARD_VENDORIMAGE_PARTITION_SIZE := 268435456
 #BOARD_VENDOR_SEPOLICY_DIRS       += device/renesas/$(TARGET_PRODUCT)/sepolicy/vendor
 
 # Boot image
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-BOARD_DTBIMAGE_PARTITION_SIZE := 1048576
-BOARD_DTBOIMG_PARTITION_SIZE := 524288
+#BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+#BOARD_DTBIMAGE_PARTITION_SIZE := 1048576
+#BOARD_DTBOIMG_PARTITION_SIZE := 524288
 
 # Root image
 TARGET_COPY_OUT_ROOT := root
@@ -67,11 +67,13 @@ BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 TARGET_COPY_OUT_RECOVERY := recovery
 
 # Kernel build rules
+KERNEL_CONFIGS_DIR:= ../configs/p/android-4.14
 BOARD_KERNEL_BASE     := 0x40008000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS  := --second_offset 0x800 --kernel_offset 0x80000 --ramdisk_offset 0x2180000
 BOARD_KERNEL_CMDLINE  := console=ttySC0,115200 init=/init androidboot.console=ttySC0 androidboot.hardware=orangepi_plus2e androidboot.selinux=permissive
 TARGET_KERNEL_SOURCE  := kernel/allwinner
+#TARGET_KERNEL_CONFIG  := orangepi_plus2e_defconfig ${KERNEL_CONFIGS_DIR}/android-base.cfg ${KERNEL_CONFIGS_DIR}/android-base-arm.cfg ${KERNEL_CONFIGS_DIR}/android-recommended.cfg
 TARGET_KERNEL_CONFIG  := orangepi_plus2e_defconfig android-base.config android-recommended.config
 
 # SELinux support
