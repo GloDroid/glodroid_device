326375ac198664492ac49e460446661a9a508657
Roman Stratiienko <roman.stratiienko@globallogic.com>
2019-07-21 13:46:29 +0300

    plus2e: BoardConfig: Use armv7-a-neon instead of armv7-a target
    This will enable several benefits:
    1. Performance improvements
    2. Android-Q does not support armv7-a
    
    Signed-off-by: Roman Stratiienko <roman.stratiienko@globallogic.com>

--- plus2e/BoardConfig.mk
+++ plus2e/BoardConfig.mk
@@ -20,7 +20,7 @@ TARGET_NO_KERNEL := false
 
 # Architecture
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a
+TARGET_ARCH_VARIANT := armv7-a-neon
 #TARGET_CPU_VARIANT := cortex-a7
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_ABI := armeabi-v7a
