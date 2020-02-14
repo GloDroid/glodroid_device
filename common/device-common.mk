013bc65b9019efa417a32c6587c3088f2867bfcf
Roman Stratiienko <roman.stratiienko@globallogic.com>
2020-02-14 15:06:14 +0200

    Reduce drm_hwcomposer HAL to v2.1
    We have no benefit using 2.3, since almost no functionality implemented
    currently.
    Also it reduces amount of passed VTS tests.
    
    Signed-off-by: Roman Stratiienko <roman.stratiienko@globallogic.com>

--- common/device-common.mk
+++ common/device-common.mk
@@ -99,8 +99,8 @@ PRODUCT_PACKAGES += \
     gralloc.gbm \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.3-impl \
-    android.hardware.graphics.composer@2.3-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
 
 PRODUCT_PROPERTY_OVERRIDES += \
