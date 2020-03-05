ea0f04ba87ac3f4bc3aba158aa88c7c1e028378b
Roman Stratiienko <r.stratiienko@gmail.com>
2020-03-05 10:08:48 +0200

    Add orange-pi3 product configuration
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

---
+++ opi3/device.mk
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# Copyright (C) 2020 Roman Stratiienko (r.stratiienko@gmail.com)
+
+$(call inherit-product, device/glodroid/common/device-common.mk)
+
+# tools
+PRODUCT_COPY_FILES += \
+    device/glodroid/platform/tools/gensdimg.sh:$(TARGET_COPY_OUT)/gensdimg.sh
---
+++ opi3/opi3.mk
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# Copyright (C) 2020 Roman Stratiienko (r.stratiienko@gmail.com)
+
+$(call inherit-product, device/glodroid/opi3/device.mk)
+
+PRODUCT_BOARD_PLATFORM ?= H6
+PRODUCT_NAME := opi3
+PRODUCT_DEVICE := opi3
+PRODUCT_BRAND := OrangePI
+PRODUCT_MODEL := opi3
+PRODUCT_MANUFACTURER := xunlong
+
+UBOOT_DEFCONFIG := orangepi_one_plus_defconfig
+ATF_PLAT        := sun50i_h6
+
+KERNEL_DEFCONFIG := defconfig
+KERNEL_FRAGMENTS := \
+    device/glodroid/platform/kernel/android-sunxi.config \
+
+KERNEL_DTB_FILE := allwinner/sun50i-h6-orangepi-3.dtb
