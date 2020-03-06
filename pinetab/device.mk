7fffa41dbe359eeaf47042e842f4df77a2d6ada9
Icenowy Zheng <icenowy@aosc.io>
2020-03-06 17:35:29 +0800

    pinetab: hack for dri/card1
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

--- pinetab/device.mk
+++ pinetab/device.mk
@@ -5,6 +5,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, device/glodroid/common/device-common.mk)
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/drm.pinetab.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/drm.pinetab.rc \
+
 # tools
 PRODUCT_COPY_FILES += \
     device/glodroid/platform/tools/gensdimg.sh:$(TARGET_COPY_OUT)/gensdimg.sh
