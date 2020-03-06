7b450878c406ee96e5094be89764b2ab98172f4d
Icenowy Zheng <icenowy@aosc.io>
2020-03-06 15:00:59 +0800

    add PineTab board
    PineTab is a tablet by Pine64, powered by Allwinner A64.
    
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

---
+++ pinetab/device.mk
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# Copyright (C) 2020 Roman Stratiienko (r.stratiienko@gmail.com)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, device/glodroid/common/device-common.mk)
+
+# tools
+PRODUCT_COPY_FILES += \
+    device/glodroid/platform/tools/gensdimg.sh:$(TARGET_COPY_OUT)/gensdimg.sh
---
+++ pinetab/pinetab.mk
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# Copyright (C) 2020 Roman Stratiienko (r.stratiienko@gmail.com)
+
+$(call inherit-product, device/glodroid/pinetab/device.mk)
+
+PRODUCT_BOARD_PLATFORM ?= A64
+PRODUCT_NAME := pinetab
+PRODUCT_DEVICE := pinetab
+PRODUCT_BRAND := Pine64
+PRODUCT_MODEL := pinetab
+PRODUCT_MANUFACTURER := pine64
+
+UBOOT_DEFCONFIG := sopine_baseboard_defconfig
+ATF_PLAT        := sun50i_a64
+
+KERNEL_DEFCONFIG := device/glodroid/platform/common/sunxi/sunxi64_defconfig
+KERNEL_FRAGMENTS := \
+    device/glodroid/platform/kernel/android-sunxi.config \
+    device/glodroid/pinetab/kernel.config \
+
+KERNEL_DTB_FILE := allwinner/sun50i-a64-pinetab.dtb
