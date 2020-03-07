9908b475309394243f9992f237231af1d3975b6f
Icenowy Zheng <icenowy@aosc.io>
2020-03-07 22:21:26 +0800

    pinetab: override some device info
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

--- pinetab/device.mk
+++ pinetab/device.mk
@@ -5,6 +5,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, device/glodroid/common/device-common.mk)
 
+PRODUCT_CHARACTERISTICS := tablet
+PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=151
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/drm.pinetab.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/drm.pinetab.rc \
 
