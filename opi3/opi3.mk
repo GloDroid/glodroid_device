2efbf8256e1376ab5e576540bcf2d70acc6663b6
Roman Stratiienko <r.stratiienko@gmail.com>
2020-03-12 12:20:51 +0200

    Make WIFI module for opi3 built in kernel.
    Temporary solution while loadable modules not supported.
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- opi3/opi3.mk
+++ opi3/opi3.mk
@@ -18,6 +18,7 @@ ATF_PLAT        := sun50i_h6
 KERNEL_DEFCONFIG := device/glodroid/platform/common/sunxi/sunxi64_defconfig
 KERNEL_FRAGMENTS := \
     device/glodroid/platform/common/sunxi/sunxi-common.config \
+    $(LOCAL_PATH)/kernel.config \
 
 KERNEL_DTB_FILE := allwinner/sun50i-h6-orangepi-3.dtb
 
