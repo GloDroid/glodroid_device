cc1e9ce4cca84318f1da2e5f46b001b48d1f7861
Roman Stratiienko <r.stratiienko@gmail.com>
2020-03-12 21:12:51 +0200

    Enable Bluetooth for opi3
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- opi3/device.mk
+++ opi3/device.mk
@@ -7,3 +7,9 @@ $(call inherit-product, device/glodroid/common/device-common.mk)
 # tools
 PRODUCT_COPY_FILES += \
     device/glodroid/platform/tools/gensdimg.sh:$(TARGET_COPY_OUT)/gensdimg.sh
+
+# Bluetooth
+PRODUCT_PACKAGES += android.hardware.bluetooth@1.0-service.btlinux
+PRODUCT_COPY_FILES += \
+        frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+        frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
