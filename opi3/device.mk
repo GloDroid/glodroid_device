e2434ae50d779f87586f8a564affdf3c09cd95bc
Roman Stratiienko <r.stratiienko@gmail.com>
2020-03-13 16:01:37 +0200

    opi3: Copy firmware for Bluetooth and WIFI to vendor image.
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- opi3/device.mk
+++ opi3/device.mk
@@ -13,3 +13,9 @@ PRODUCT_PACKAGES += android.hardware.bluetooth@1.0-service.btlinux
 PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
         frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
+
+# Firmware
+PRODUCT_COPY_FILES += \
+        kernel/firmware/brcm/BCM4345C5.hcd:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/brcm/BCM4345C5.hcd \
+        kernel/firmware/brcm/brcmfmac43456-sdio.bin:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/brcm/brcmfmac43456-sdio.bin \
+        kernel/firmware/brcm/brcmfmac43456-sdio.txt:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/brcm/brcmfmac43456-sdio.txt \
