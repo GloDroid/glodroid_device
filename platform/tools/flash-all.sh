fb8cfd9587140d6b5cf8792436c1b0e8524d0828
Roman Stratiienko <r.stratiienko@gmail.com>
2020-03-16 22:59:53 +0200

    Add simple script for flashing over fastbootd
    Usage:
    $ cd out/target/product/<board>/
    $ adb reboot fastboot
    $ ./flash-all.sh
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

---
+++ platform/tools/flash-all.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -xe
+
+fastboot flash env      env.img
+fastboot flash misc     misc.img
+fastboot flash boot_a   boot.img
+fastboot flash dtbo_a   boot_dtbo.img
+fastboot flash metadata metadata.img
+fastboot flash system   system.img
+fastboot flash vendor   vendor.img
+fastboot flash product  product.img
+fastboot reboot
