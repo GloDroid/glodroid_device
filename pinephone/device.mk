f11bec91039306a090ce2b771c19cf5fc7a0fea0
Icenowy Zheng <icenowy@aosc.io>
2020-04-06 21:13:36 +0800

    pinephone: enable sensors with sensors.iio hal
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

--- pinephone/device.mk
+++ pinephone/device.mk
@@ -17,6 +17,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi.pinephone.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/wifi.pinephone.rc \
     $(LOCAL_PATH)/lights.pinephone.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/lights.pinephone.rc \
     $(LOCAL_PATH)/vibrator.pinephone.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vibrator.pinephone.rc \
+    $(LOCAL_PATH)/sensors.pinephone.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/sensors.pinephone.rc \
 
 PRODUCT_COPY_FILES += \
     device/glodroid/pinephone/audio.pinephone.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio.pinephone.xml \
@@ -29,6 +30,11 @@ PRODUCT_PACKAGES += \
     lights.pinephone \
     android.hardware.light@2.0-impl:64 \
     android.hardware.light@2.0-service
+
+PRODUCT_PACKAGES += \
+    sensors.iio \
+    android.hardware.sensors@1.0-impl:64 \
+    android.hardware.sensors@1.0-service
 
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service.pinephone
