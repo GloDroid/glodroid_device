f119f140abf888cb3f2cb3e7d0f89c5111d6573c
Roman Stratiienko <r.stratiienko@gmail.com>
2020-06-22 10:01:25 +0300

    plus2: Enable WIFI
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- opi_plus2/device.mk
+++ opi_plus2/device.mk
@@ -6,5 +6,12 @@ $(call inherit-product, device/glodroid/common/device-common.mk)
 $(call inherit-product, device/glodroid/common/device-common-sunxi.mk)
 $(call inherit-product, device/glodroid/common/bluetooth/no-bluetooth.mk)
 
+# Out-of-tree modules
+PRODUCT_PACKAGES += \
+    8189es.ko \
+
 PRODUCT_COPY_FILES += \
     device/glodroid/opi_plus2e/audio.opi_plus2e.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio.opi_plus2.xml \
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi.plus2.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/wifi.plus2.rc \
