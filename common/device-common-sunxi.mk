519b99bb54bbbb35bf81e7ba10186a8d01b5c440
Roman Stratiienko <r.stratiienko@gmail.com>
2020-06-27 16:39:58 +0300

    Enable using drm_hwcomposer for layer composition on RPI4
    RPI4 HWC significantly speeds-up UI and seems it is working without issues.
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- common/device-common-sunxi.mk
+++ common/device-common-sunxi.mk
@@ -5,3 +5,8 @@ PRODUCT_COPY_FILES += \
 
 UBOOT_FRAGMENTS += \
     device/glodroid/platform/common/sunxi/uboot.config
+
+# SUNXI has broken drm/sun4i DE2 kernel driver.
+# Disable drm_hwcomposer for layer composition.
+PRODUCT_PROPERTY_OVERRIDES += \
+    setprop hwc.drm.exclude_non_hwfb_imports=1
