5e441440397e5dcde7516ba1ef51ac30e08daa1d
Roman Stratiienko <r.stratiienko@gmail.com>
2020-07-01 12:25:24 +0300

    common: Reduce keymaster HAL version to 3.0
    KeyMaster v4.0 fails a lot of VTS, which passes on v3.0.
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- common/device-common.mk
+++ common/device-common.mk
@@ -131,8 +131,8 @@ PRODUCT_PACKAGES += \
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.0-impl \
-    android.hardware.keymaster@4.0-service \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service \
     keystore.ranchu
 
 # Health HAL
