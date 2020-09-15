2d674a787d8ac496f9a89ac80bc21a77140d032e
Roman Stratiienko <r.stratiienko@gmail.com>
2020-09-15 17:03:16 +0300

    R: Replace Thermal 1.0 with Thermal 2.0-MOCK (Temporary)
    Thermal 1.0 can't be used with R, but there is no functional replacement.
    Use MOCK until we found better solution.
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- common/device-common.mk
+++ common/device-common.mk
@@ -112,9 +112,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal HAL
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.default
+    android.hardware.thermal@2.0-service.mock \
 
 # Graphics HAL
 PRODUCT_PACKAGES += \
