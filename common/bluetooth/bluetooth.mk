909b38f6d151d9cf59c5cb87adc041343c25471d
Roman Stratiienko <r.stratiienko@gmail.com>
2020-09-16 19:40:58 +0300

    R: Upgrade btlinux HAL to @1.1
    @1.0 implementation is missing in starting from Android-11
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- common/bluetooth/bluetooth.mk
+++ common/bluetooth/bluetooth.mk
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
-PRODUCT_PACKAGES += android.hardware.bluetooth@1.0-service.btlinux
+PRODUCT_PACKAGES += android.hardware.bluetooth@1.1-service.btlinux
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
