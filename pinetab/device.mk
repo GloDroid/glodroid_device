932960d8be27350ba9389a87bf919357e63acf76
Roman Stratiienko <r.stratiienko@gmail.com>
2020-10-21 21:31:38 +0300

    pinetab: Align kernel configs with v5.9 + enable wifi
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- pinetab/device.mk
+++ pinetab/device.mk
@@ -10,5 +10,10 @@ $(call inherit-product, device/glodroid/common/bluetooth/no-bluetooth.mk)
 PRODUCT_CHARACTERISTICS := tablet
 PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=151
 
+# Firmware
+PRODUCT_COPY_FILES += \
+    kernel/firmware/rtlbt/rtl8723cs_xx_fw:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/rtl_bt/rtl8723cs_xx_fw.bin \
+    kernel/firmware/rtlbt/rtl8723cs_xx_config:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/rtl_bt/rtl8723cs_xx_config-pinetab.bin \
+
 DEVICE_PACKAGE_OVERLAYS += \
     device/glodroid/pinetab/overlay
