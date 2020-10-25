75f0cef694847bcbd2ad46a9bce5ffe202096ef1
Roman Stratiienko <r.stratiienko@gmail.com>
2020-10-25 19:21:07 +0200

    common: remove build rule which removes configstore service
    Starting from Android-11 ConfigStore service isn't included
    into the build if SHIPPING API >= 30.
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- common/Android.mk
+++ common/Android.mk
@@ -25,9 +25,3 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                         := remove-Bluetooth
 EXECUTABLES.remove-Bluetooth.OVERRIDES := Bluetooth
 include $(BUILD_PHONY_PACKAGE)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE                         := remove-android.hardware.configstore@1.1-service
-EXECUTABLES.remove-android.hardware.configstore@1.1-service.OVERRIDES := \
-					android.hardware.configstore@1.1-service
-include $(BUILD_PHONY_PACKAGE)
