48477d193c780da404adf1a7fa7b572bc2cd35bb
Roman Stratiienko <r.stratiienko@gmail.com>
2021-04-26 12:42:43 +0300

    pinephone: Switch to @megous defconfig
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- pinephone/pinephone.mk
+++ pinephone/pinephone.mk
@@ -15,10 +15,8 @@ PRODUCT_HAS_EMMC := true
 UBOOT_DEFCONFIG := pinephone_defconfig
 ATF_PLAT        := sun50i_a64
 
-KERNEL_DEFCONFIG := device/glodroid/platform/common/sunxi/sunxi64_defconfig
-KERNEL_FRAGMENTS := \
-    device/glodroid/platform/common/sunxi/sunxi-common.config \
-    device/glodroid/pinephone/kernel.config \
+KERNEL_DEFCONFIG := kernel/glodroid-sunxi/arch/arm64/configs/pinephone_defconfig
+KERNEL_FRAGMENTS := device/glodroid/pinephone/kernel.config
 
 KERNEL_DTB_FILE_PP11 := allwinner/sun50i-a64-pinephone-1.1.dtb
 KERNEL_DTB_FILE_PP12 := allwinner/sun50i-a64-pinephone-1.2.dtb
