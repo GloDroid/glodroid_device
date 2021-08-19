3aa049b01245dc8aa9338634613bf12757181cb6
Roman Stratiienko <r.stratiienko@gmail.com>
2021-08-19 17:28:05 +0300

    platform: crust: Copy sources into out directory before build
    After upgading to Ubuntu 21.04, problem related to crust's Kconfig
    started to appear. Fixed by copying sources and then build all in-place.
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- platform/crust.mk
+++ platform/crust.mk
@@ -11,8 +11,11 @@ CRUST_FIRMWARE_SRC    ?= external/crust-firmware/crust
 OR1K_TOOLCHAIN_PREFIX := prebuilts/gcc/linux-x86/or1k/gcc-linux-or1k/bin/or1k-linux-
 OR1K_COMPILE          := CROSS_COMPILE=$(AOSP_TOP_ABS)/$(OR1K_TOOLCHAIN_PREFIX)
 
-SCP_MAKE := PATH=/usr/bin:/bin:/sbin:$$PATH $(OR1K_COMPILE) $(MAKE) -C $(CRUST_FIRMWARE_SRC) OBJ=$(AOSP_TOP_ABS)/$(CRUST_FIRMWARE_OUT)
+SCP_MAKE := PATH=/usr/bin:/usr/sbin:/bin:/sbin $(OR1K_COMPILE) $(MAKE) -C $(CRUST_FIRMWARE_OUT)
 
 $(CRUST_FIRMWARE_BINARY): $(sort $(shell find -L $(CRUST_FIRMWARE_SRC)))
+	rm -rf $(CRUST_FIRMWARE_OUT)
+	mkdir -p $(CRUST_FIRMWARE_OUT)
+	cp -r $(CRUST_FIRMWARE_SRC)/* $(CRUST_FIRMWARE_OUT)
 	$(SCP_MAKE) $(CRUST_FIRMWARE_DEFCONFIG)
 	$(SCP_MAKE) scp
