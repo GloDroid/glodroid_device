5c258a304994e69e7257341e889953f1e728126b
Roman Stratiienko <r.stratiienko@gmail.com>
2021-08-23 00:23:46 +0300

    common: Add libcamera and dependencies
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- common/device-common.mk
+++ common/device-common.mk
@@ -208,11 +208,24 @@ endif
 
 #External USB Camera HAL
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-external-service \
+    android.hardware.camera.provider@2.5-external-service \
 
 PRODUCT_COPY_FILES += \
     device/glodroid/common/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+
+#Camera HAL
+PRODUCT_PACKAGES += \
+    camera.libcamera \
+    android.hardware.camera.provider@2.5-service_64 \
+
+PRODUCT_PROPERTY_OVERRIDES += ro.hardware.camera=libcamera
+
+PRODUCT_COPY_FILES +=  \
+    frameworks/native/data/etc/android.hardware.camera.concurrent.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.concurrent.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
+    frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml \
 
 # Copy hardware config file(s)
 PRODUCT_COPY_FILES +=  \
