79d5925f9c678dd173d92971631ee3fd3a3ae200
Roman Stratiienko <r.stratiienko@gmail.com>
2021-10-06 13:48:02 +0300

    Upgrade fastboot recovery HAL to v1.1
    Required by Android-12
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- common/device-common.mk
+++ common/device-common.mk
@@ -90,7 +90,7 @@ PRODUCT_PACKAGES += \
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd \
-    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.1-impl-mock \
 
 # Boot control
 PRODUCT_PACKAGES += \
