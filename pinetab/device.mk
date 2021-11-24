42c55e2ad6e5e56fb0a37b35c877f4e8bfbb5d38
Roman Stratiienko <r.stratiienko@gmail.com>
2021-11-24 09:52:29 +0200

    pinetab: Use qemu.sf.lcd_density instead of ro.sf.lcd_density
    qemu.sf.lcd_density wil override the density at runtime.
    
    Fixes the error:
    
        error: found duplicate sysprop assignments:
        ro.sf.lcd_density=160
        ro.sf.lcd_density=160
        ro.sf.lcd_density=151
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- pinetab/device.mk
+++ pinetab/device.mk
@@ -8,7 +8,7 @@ $(call inherit-product, device/glodroid/common/device-common-sunxi.mk)
 $(call inherit-product, device/glodroid/common/bluetooth/no-bluetooth.mk)
 
 PRODUCT_CHARACTERISTICS := tablet
-PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=151
+PRODUCT_PROPERTY_OVERRIDES += qemu.sf.lcd_density=151
 
 # Firmware
 PRODUCT_COPY_FILES += \
