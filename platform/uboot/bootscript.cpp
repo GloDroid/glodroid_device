88434bfa8cae5219dcc8fd48a94379543d3c74e6
Roman Stratiienko <r.stratiienko@gmail.com>
2022-04-20 20:09:54 +0300

    Add reserved memory region for PPP
    SAD. the tee we currently have doesnt reserve its memory in the
    bootloader. Reserve it manually.
    
    Signed-off-by: Pierre-Hugues Husson <phh@phh.me>
    [RomanS: wrap into ifdef]
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- platform/uboot/bootscript.cpp
+++ platform/uboot/bootscript.cpp
@@ -125,6 +125,10 @@ FUNC_BEGIN(bootcmd_start)
  adtimg get dt --id=\$overlay_fdt_id dtb_start dtb_size overlay_fdt_index &&
  cp.b \$dtb_start \$dtboaddr \$dtb_size &&
  fdt resize 8192 &&
+#ifdef device_pinephonepro
+ fdt rsvmem add 0x8000000 0x8000000 &&
+ fdt rsvmem print &&
+#endif
  fdt apply \$dtboaddr &&
  FEXTENV(bootargs, " androidboot.dtbo_idx=\${main_fdt_index},\${overlay_fdt_index}") ;
  /* START KERNEL */
