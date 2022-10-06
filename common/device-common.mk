c7fe584b6badb846f0dee379494a4e8ecf9cecff
Roman Stratiienko <r.stratiienko@gmail.com>
2022-10-06 20:59:19 +0300

    common: Bump audio-effects to v7.1
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- common/device-common.mk
+++ common/device-common.mk
@@ -65,7 +65,7 @@ PRODUCT_PACKAGES += audio.usb.default \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@7.0-impl \
+    android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
 
 PRODUCT_COPY_FILES += \
